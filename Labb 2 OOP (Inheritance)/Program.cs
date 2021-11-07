7ffb4fc759ffbbae96f5aaad5c2045e2e9b21318
Theo Andrén <theo.andren@gmail.com>
2021-11-07 15:18:10 +0100

    Finished.

--- Labb 2 OOP (Inheritance)/Program.cs
+++ Labb 2 OOP (Inheritance)/Program.cs
@@ -5,18 +5,26 @@ namespace Labb_2_OOP__Inheritance_
     public class Djur
     {
         public string Name;
-        public string Age;
+        public int Age;
         public string Color;
-        public string NumberOfLegs;
-        public string NumberOfEyes;
+        public int NumberOfLegs;
+        public int NumberOfEyes;
 
-        public Djur(string name, string age, string color, string numberoflegs, string numberofeyes)
+        public Djur(string name, int age, string color, int numberoflegs, int numberofeyes)
         {
             this.Name = name;
             this.Age = age;
             this.Color = color;
             this.NumberOfLegs = numberoflegs;
             this.NumberOfEyes = numberofeyes;
+        }
+        public Djur(string name, int age, string color)
+        {
+            this.NumberOfLegs = 4;
+            this.NumberOfEyes = 2;
+            this.Name = name;
+            this.Age = age;
+            this.Color = color;
         }
 
 
@@ -38,6 +46,10 @@ namespace Labb_2_OOP__Inheritance_
     public class Dog : Djur
     {
         public string CollarColor;
+        public Dog(string collarcolor, string color,string name, int age) : base(name,age,color)
+        {
+            this.CollarColor = collarcolor;
+        }
 
         public void Commands()
         {
@@ -49,9 +61,15 @@ namespace Labb_2_OOP__Inheritance_
         }
 
     }
-    class Cat : Djur
+    public class Cat : Djur
     {
         public string NecklaceColor;
+        
+        public Cat(string necklacecolor, string color,string name, int age) : base(name, age, color)
+        {
+            this.NecklaceColor = necklacecolor;
+
+        }
 
         public void Spin()
         {
@@ -66,8 +84,12 @@ namespace Labb_2_OOP__Inheritance_
     }
     public class Mouse : Djur
     {
-
         public string ColorOfEyes;
+        
+        public Mouse(string colorofeyes, string color,string name, int age) : base(name, age, color)
+        {
+            this.ColorOfEyes = colorofeyes;
+        }
 
         public void Stare()
         {
@@ -82,6 +104,11 @@ namespace Labb_2_OOP__Inheritance_
     public class Bulldog : Dog
     {
         public string Weight;
+        public Bulldog(string CollarColor,string weight,string name, int age) : base(CollarColor,"White",name,age)
+        {
+            this.Weight = weight;
+        }
+        
 
         public void Askforfood()
         {
@@ -96,6 +123,12 @@ namespace Labb_2_OOP__Inheritance_
     public class Golden_Retriver : Dog
     {
         public string Length;
+        public Golden_Retriver(string CollarColor, string length, string name, int age) : base(CollarColor,"Brown",name,age)
+        {
+            this.Length = length;
+        }
+
+       
 
         public void Run()
         {
@@ -112,20 +145,26 @@ namespace Labb_2_OOP__Inheritance_
     {
         static void Main(string[] args)
         {
-            Dog Dog = new Dog();
+            Dog Dog = new Dog("Red","Brown","Teddy",5);
             Dog.MakeSound();
 
-            Cat Cat = new Cat();
+            Cat Cat = new Cat("Blue","Black","Pär",3);
             Cat.MakeSound();
 
-            Mouse Mouse = new Mouse();
+            Mouse Mouse = new Mouse("Red","Grey","Åke",2);
             Mouse.MakeSound();
 
-            Bulldog Bulldog = new Bulldog();
+            Bulldog Bulldog = new Bulldog("Green","White","Sven",1);
             Bulldog.MakeSound();
 
-            Golden_Retriver Golden_Retriver = new Golden_Retriver();
+            Golden_Retriver Golden_Retriver = new Golden_Retriver("Yellow","Brown","Olof",7);
             Golden_Retriver.MakeSound();
+
+            Djur Fox = new Djur("Otto",4,"Red",8,2);
+            Fox.MakeSound();
+
+
+
 
         }
     }
